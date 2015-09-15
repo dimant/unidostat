1415c4a38585f47899098848d215e09c3bfb9ec1
unknown <diman@DESKTOP-UP4LBD9.europe.corp.microsoft.com>
2015-09-15 01:30:21 -0700

    added runner

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 var httpServer = require('./modified_modules/http-server.js');
 
+var log = console.log;
+
+var requestLogger = function (req, res, error) {
+    var date = (new Date).toUTCString();
+    if (error) {
+		log('[%s] "%s %s" Error (%s): "%s"', date, req.method, req.url, error.status.toString(), error.message);
+    } else {
+		log('[%s] "%s %s" "%s"', date, req.method, req.url, req.headers['user-agent']);
+    }
+};
+
 var proxyServer = httpServer.createServer({
 	proxy: 'https://stat.unido.org:443/',
 	cors: true,
 	https: {
-		cert:'diman_todorov_cert.pem',
-		key:'diman_todorov_key.pem',
-	}
+		cert: 'diman_todorov_cert.pem',
+		key: 'diman_todorov_key.pem',
+	},
+	logFn: requestLogger
 });
 proxyServer.listen(8123);
 
 var server = httpServer.createServer({
 	root: './app',
 	https: {
-		cert:'diman_todorov_cert.pem',
-		key:'diman_todorov_key.pem',
-	}
+		cert: 'diman_todorov_cert.pem',
+		key: 'diman_todorov_key.pem',
+	},
+	logFn: requestLogger
 });
 server.listen(8000);
